dc7417b20ad46fa25684999f5ac16383b584f789
Leina33 <dominickrutto@gmail.com>
2019-10-02 14:44:53 +0300

    comment a hover to rectify it with another hover jquery

--- Js/scripts.js
+++ Js/scripts.js
@@ -5,3 +5,23 @@ $(document).ready(function(){
     $("h4").click(function(){
         $(".q").toggle();
     });
+    $("h5").click(function(){
+        $(".k").toggle();
+    });
+
+    $(".image").hover(function(){
+      $(this).animate({opacity: "0.4"});
+    } ,);
+  });
+  $(document).ready(function(){
+    $("form").submit(function(event){
+      var name = $("#name").val();
+      var email = $("#email").val();
+      var message = $("#message").val();
+      event.preventDefault();
+      if ( name && email != '')
+      {
+        alert("Thank you " + name +  "thank you for reaching us");
+      }
+    });
+  });
